735724d3e2dff8f307d16abedfb6fbf0d499e144
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-09-26 10:51:15 +0200

    feat : debut formulaire choix fichier.csv

--- src/vue/utilisateur/vueImportationCSV.php
+++ src/vue/utilisateur/vueImportationCSV.php
@@ -1,1 +1,7 @@
-<?php
+<form action="">
+    <div>
+        <label for="choixFichier">Choisissez votre fichier (.csv) : </label>
+        <input type="file" id="choixFichier" name="choixFichier" accept=".csv">
+        <button type="submit">Valider</button>
+    </div>
+</form>
