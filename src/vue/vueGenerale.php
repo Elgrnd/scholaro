06fccb8aef84c9a8e51d08f3af4e1f67a4b89395
Gabin <gabin.laurent01@etu.umontpellier.fr>
2024-10-10 00:14:25 +0200

    feat navbar

--- src/vue/vueGenerale.php
+++ src/vue/vueGenerale.php
@@ -12,9 +12,31 @@
          */
         ?></title>
     <link href="../ressources/css/<?= str_replace(".php", "Style.css", $cheminCorpsVue)?>" rel="stylesheet">
+    <link rel="stylesheet" href="../ressources/css/vueGeneraleStyle.css" />
 </head>
 <body>
 <header>
+
+    <nav class="navbar">
+        <img class="logo" src="../ressources/images/logo_IUT.png" alt="logo" />
+        <div class="nav_links">
+            <ul>
+                <li>
+                    <a href="controleurFrontal.php?action=afficherListe">ACCUEIL</a>
+                </li>
+                <li>
+                    <a href="controleurFrontal.php?action=afficherListe">LISTE</a>
+                </li>
+                <li>
+                    <a href="#">IMPORTATION</a>
+                </li>
+                <li>
+                    <a href="#"><img class="logout" src="../ressources/images/logout.png" alt="se déconnecter"/></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
 </header>
 <main>
     <?php
