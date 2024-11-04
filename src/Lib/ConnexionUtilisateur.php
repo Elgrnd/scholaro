c975efc2c104fe15ffde27f64e06d29dc46b21e4
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-11-04 16:01:26 +0100

    feat : connexion d'un etudiant avec son etudid et son code nip en mdp par défaut

---
+++ src/Lib/ConnexionUtilisateur.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Sae\Lib;
+
+use App\Sae\Modele\HTTP\Session;
+
+class ConnexionUtilisateur
+{
+    // L'utilisateur connecté sera enregistré en session associé à la clé suivante
+    private static string $cleConnexion = "_utilisateurConnecte";
+
+    public static function connecter(string $loginUtilisateur): void
+    {
+        $session = Session::getInstance();
+        $session->enregistrer(ConnexionUtilisateur::$cleConnexion, $loginUtilisateur);
+    }
+
+    public static function estConnecte(): bool
+    {
+        $session = Session::getInstance();
+        return $session->contient(ConnexionUtilisateur::$cleConnexion);
+    }
+
+    public static function deconnecter(): void
+    {
+        $session = Session::getInstance();
+        $session->supprimer(ConnexionUtilisateur::$cleConnexion);
+    }
+
+    public static function getLoginUtilisateurConnecte(): ?string
+    {
+        return $_SESSION[ConnexionUtilisateur::$cleConnexion];
+    }
+
+    public static function estUtilisateur($login): bool {
+        return self::estConnecte() && self::getLoginUtilisateurConnecte() == $login;
+    }
+}
