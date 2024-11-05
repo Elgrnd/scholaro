cce8b78dd763aef68f1a41653675ddcd7bd53f1f
Gabin <gabin.laurent01@etu.umontpellier.fr>
2024-11-05 12:16:15 +0100

    fix commentaire afficherErreur

--- src/Controleur/ControleurAgregation.php
+++ src/Controleur/ControleurAgregation.php
@@ -38,18 +38,12 @@ class ControleurAgregation extends ControleurGenerique
         }
     }
 
-    public static function afficherErreur(string $erreur): void
-    {
-        ControleurGenerique::afficherVue("vueGenerale.php", ["titre" => "Erreur", "cheminCorpsVue" => "agregation/erreur.php", "erreur" => $erreur]);
-    }
-
-
     /**
      * @param string $erreur message d'erreur à afficher
      * @return void afficher la page d'erreur
      */
-    public static function afficherErreur(string $erreur = ""): void
+    public static function afficherErreur(string $erreur): void
     {
-        ControleurGenerique::afficherVue("vueGenerale.php", ["titre" => "Erreur", "cheminCorpsVue" => "etudiant/erreur.php", "erreur" => $erreur]);
+        ControleurGenerique::afficherVue("vueGenerale.php", ["titre" => "Erreur", "cheminCorpsVue" => "agregation/erreur.php", "erreur" => $erreur]);
     }
 }
