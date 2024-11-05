c6790786c176c2c6459e3a067eff163fc8f11607
nedjary <younes.nedjar@etu.umontpellier.fr>
2024-11-05 15:07:44 +0100

    refactor formulaireConnexion style de la page

--- src/vue/formulaireConnexion.php
+++ src/vue/formulaireConnexion.php
@@ -3,20 +3,20 @@
         <legend>Connexion :</legend>
         <input type='hidden' name='action' value='connecter'>
         <p class="InputAddOn">
-            <label class="InputAddOn-item" for="login_id">Login&#42;</label>
-            <input class="InputAddOn-field" type="text" placeholder="Ex : leblancj" name="login" id="login_id" required>
+            <label class="InputAddOn-item" for="login_id">Login</label>
+            <input class="InputAddOn-field" type="text" placeholder="Ex : pallejax" name="login" id="login_id" required>
         </p>
         <p class="InputAddOn">
-            <label class="InputAddOn-item" for="mdp_id">Mot de passe&#42;</label>
+            <label class="InputAddOn-item" for="mdp_id">Mot de passe</label>
             <input class="InputAddOn-field" type="password" value="" placeholder="" name="mdp" id="mdp_id" required>
         </p>
         <p class="InputAddOn">
-            <input class="InputAddOn-field" type="radio" id="etudiantId" name="choix_controleur" value="utilisateur">
             <label class="InputAddOn-item" for="etudiantId">Étudiant</label>
+            <input class="InputAddOn-field" type="radio" id="etudiantId" name="choix_controleur" value="utilisateur">
         </p>
         <p class="InputAddOn">
-            <input class="InputAddOn-field" type="radio" id="professeurId" name="choix_controleur" value="professeur">
             <label class="InputAddOn-item" for="professeurId">Professeur</label>
+            <input class="InputAddOn-field" type="radio" id="professeurId" name="choix_controleur" value="professeur">
         </p>
         <p>
             <input type="submit" value="Envoyer" />
