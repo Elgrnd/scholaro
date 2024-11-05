67e764df8beaf867e6b921992e35af2b5a4f8f4b
nedjary <younes.nedjar@etu.umontpellier.fr>
2024-11-05 13:54:04 +0100

    refactor formulaireConnexion supprimer boutons radio...

--- src/vue/formulaireConnexion.php
+++ src/vue/formulaireConnexion.php
@@ -2,7 +2,6 @@
     <fieldset>
         <legend>Connexion :</legend>
         <input type='hidden' name='action' value='connecter'>
-        <input type="hidden" name="controleur" value="utilisateur">
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="login_id">Login&#42;</label>
             <input class="InputAddOn-field" type="text" placeholder="Ex : leblancj" name="login" id="login_id" required>
@@ -13,14 +12,6 @@
         </p>
         <p>
             <input type="submit" value="Envoyer" />
-        </p>
-        <input type='hidden' name='action' value='enregistrerPreference'>
-        <input type="radio" id="utilisateurId" name="controleur_defaut" value="utilisateur">
-        <label for="utilisateurId">Utilisateur</label>
-        <input type="radio" id="trajetId" name="controleur_defaut" value="trajet">
-        <label for="trajetId">Trajet</label>
-        <p class="InputAddOn">
-            <input class="InputAddOn-field" type="submit" value="Envoyer" />
         </p>
     </fieldset>
 </form>
